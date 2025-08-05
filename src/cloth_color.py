c3b042764b648c10fb67b32c01efcde11ebc2395
Chaerimy0907 <qkrcofus1026@daum.net>
2025-08-05 16:10:44 +0900

    코드 수정

--- src/cloth_color.py
+++ src/cloth_color.py
@@ -16,27 +16,6 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# 색상
-color_map = {
-    ord('1') : 'Red',
-    ord('2') : 'Blue',
-    ord('3') : 'Green',
-    ord('4') : 'Yellow',
-    ord('5') : 'Black',
-    ord('6') : 'White',
-    ord('7') : 'Gray'
-}
-
-# 마우스 이벤트
-click_color = None
-def mouse_event(event, x, y, flags, param):
-    global click_color
-    if event == cv2.EVENT_LBUTTONDOWN:
-        frame = param
-        b, g, r = frame[y, x]
-        click_color = (r, g, b) # RGB로 저장
-        
 # 웹캠 연결
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
