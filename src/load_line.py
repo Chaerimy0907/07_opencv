30c776bbc0ec7d44555c12fad55d2ee1b2041328
Chaerimy0907 <qkrcofus1026@daum.net>
2025-08-05 13:04:40 +0900

    시각화 및 파이차트 추가

--- src/load_line.py
+++ src/load_line.py
@@ -14,7 +14,7 @@ if w > max_width:
 data = img.reshape((-1, 3)).astype(np.float32)
 
 # 군집합 개수
-K = 3
+K = 8
 
 # KMeans 클러스터링
 # 반복 중지 조건
@@ -23,7 +23,11 @@ ret, label, center = cv2.kmeans(data, K, None, criteria, 10, cv2.KMEANS_RANDOM_C
 
 # 중심값을 정수형으로 변환
 center = np.uint8(center)
+
+# 각 레이블에 해당하는 중심값으로 픽셀 값 선택
 res = center[label.flatten()]
+
+# 원본 영상의 형태로 변환
 res = res.reshape((img.shape))
 
 # 대표 색상 분석
@@ -41,4 +45,20 @@ for i in range(K):
 palette = np.zeros((50, 100 * K, 3), dtype=np.uint8)
 for i in range(K):
     palette[:, i*100:(i+1)*100] = center[i]
-    
+
+# 시각화
+merged = np.hstack((img, res))  # 원본 + 색상 단순화 이미지
+cv2.imshow('KMeans Result', merged)
+cv2.imshow('Color Palette', palette)
+
+# Matplotlib 파이차트
+colors_rgb = [center[i][::-1] / 255.0 for i in range(K)]    # BGR -> RGB
+label_text = [f'{i+1}' for i in range(K)]
+
+plt.figure(figsize=(5,5))
+plt.pie(ratios, labels=label_text, colors=colors_rgb, autopct='%1.1f%%')
+plt.title('색상 비율 분포')
+plt.show()
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
