714498bdf4e3adc29f4fa99f8f3e9c147106a912
Chaerimy0907 <qkrcofus1026@daum.net>
2025-08-05 12:44:45 +0900

    클리스터링 + 대표 색상 분석 추가

--- src/load_line.py
+++ src/load_line.py
@@ -11,3 +11,23 @@ if w > max_width:
     new_h = int(h * ratio)
     img = cv2.resize(img, (max_width, new_h))
 
+data = img.reshape((-1, 3)).astype(np.float32)
+
+# 군집합 개수
+K = 3
+
+# KMeans 클러스터링
+# 반복 중지 조건
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+ret, label, center = cv2.kmeans(data, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+# 중심값을 정수형으로 변환
+center = np.uint8(center)
+res = center[label.flatten()]
+res = res.reshape((img.shape))
+
+# 대표 색상 분석
+labels = label.flatten()
+counts = np.bincount(labels)
+total_pixels = counts.sum()
+ratios = counts / total_pixels
